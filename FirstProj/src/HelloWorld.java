1d11923215b6aac1ae0674e03fdd8890672eb6a1
Avi-Ku <142202633+Avi-Ku@users.noreply.github.com>
2023-08-14 15:41:38 +0530

    Update HelloWorld.java

--- FirstProj/src/HelloWorld.java
+++ FirstProj/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-           System.out.println("avinash kumar");
+           System.out.println("Avinash kumar");
 		System.out.println("anupam changed the code");
 	}
 
